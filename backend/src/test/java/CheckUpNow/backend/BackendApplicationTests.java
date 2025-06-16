34e0beb54c145c4f78115eb940e77249b22b20b2
KeltonxRocha <keltonrocha9@gmail.com>
2025-06-16 19:54:50 -0300

    springboot com suas dependencias
    Spring Web (Web)
    Spring Data JPA (SQL)
    Spring Security (Security)
    PostgreSQL Driver (SQL)
    Lombok (Developer Tools)

---
+++ backend/src/test/java/CheckUpNow/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package CheckUpNow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
